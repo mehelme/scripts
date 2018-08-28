4ff90cdefbfb2b4e2b70a4a1a91adc0eb37d4b6f
mehelmebehelme@gmail.com <mehelmebehelme@gmail.com>
2018-08-28 12:51:54 +0300

    fix test ssd

--- test_ssd.sh
+++ test_ssd.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-# Скрипт тестирует SSD
-# 1. Создает файл на свободном месте
-# 2. Читает весь диск sda
-# 3. Удаляет файл
-# И так пока не надоест, в syslog не должно быть ошибок
-# Скрипт изначально создан для теста диска corsiar force, который дал сбой
-
 foo() {
   c=0
   f="/tmp/test_ssd_file"
   while true; do
     c=$((c+1))
-    echo "TRY N $c $f"
+    echo "------------------------------ $(date): TRY N [$c]"
 
-    echo "WRITE"
+    echo "$(date): WRITE"
     dd if=/dev/urandom bs=4MB | pv | dd of=$f bs=4MB # write all free space
-    echo "READ"
+    echo "$(date): READ"
     dd if=/dev/sda bs=4MB | pv | dd of=/dev/null
-    echo "RM"
+    echo "$(date): RM"
     rm $f
-    exit
   done
 
 }
